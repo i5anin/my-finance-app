43e79cbef9e631bcc99ea33622d1e3ec3087f897
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-30 23:57:03 +0300

    +

--- src/views/chartData.vue
+++ src/views/chartData.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <PieChart :chartData="chartData" />
-  </div>
+  <Responsive class="w-full">
+    <template #main="{ width }">
+      <Chart
+        direction="circular"
+        :size="{ width, height: 400 }"
+        :data="data"
+        :margin="{
+          left: Math.round((width - 360)/2),
+          top: 20,
+          right: 0,
+          bottom: 20
+        }"
+        :axis="axis"
+        :config="{ controlHover: false }"
+      >
+        <template #layers>
+          <Pie
+            :dataKeys="['name', 'pl']"
+            :pie-style="{ innerRadius: 100, padAngle: 0.05 }" />
+        </template>
+        <template #widgets>
+          <Tooltip
+            :config="{
+              name: { },
+              avg: { hide: true},
+              pl: { label: 'value' },
+              inc: { hide: true }
+            }"
+            hideLine
+          />
+        </template>
+      </Chart>
+    </template>
+  </Responsive>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Chart, Responsive, Pie, Tooltip } from 'vue3-charts'
+import { plByMonth } from './data.js'
+
+export default defineComponent({
+  name: 'LineChart',
+  components: { Chart, Responsive, Pie, Tooltip },
+  setup() {
+    const data = ref(plByMonth)
+    return { data }
+  }
+})
+</script>
