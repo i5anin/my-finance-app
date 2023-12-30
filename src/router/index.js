c3453f8e9f7d2e6f9120e803cba4e6db28aead15
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-30 23:11:56 +0300

    +

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 // src/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import Tool from '@/modules/tool/components/Tabs.vue'
-import Login from '@/views/Login.vue'
+// import Tool from '@/modules/tool/components/Tabs.vue'
+// import Login from '@/views/Login.vue'
 import Error404 from '@/views/404.vue'
 
 export const Routes = {
@@ -26,24 +26,24 @@ const routes = [
     component: Home,
     // beforeEnter: authGuard.Home
   },
-  {
-    path: Routes.LOGIN,
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/Tool/:parentId',
-    name: 'ToolWithParentId',
-    component: Tool,
-    props: true,
-  },
-  // Маршрут без параметра parentId
-  {
-    path: '/Tool',
-    name: 'Tool',
-    component: Tool,
-    props: { parentId: 0 }, // Здесь мы устанавливаем parentId по умолчанию
-  },
+  // {
+  //   path: Routes.LOGIN,
+  //   name: 'Login',
+  //   component: Login,
+  // },
+  // {
+  //   path: '/Tool/:parentId',
+  //   name: 'ToolWithParentId',
+  //   component: Tool,
+  //   props: true,
+  // },
+  // // Маршрут без параметра parentId
+  // {
+  //   path: '/Tool',
+  //   name: 'Tool',
+  //   component: Tool,
+  //   props: { parentId: 0 }, // Здесь мы устанавливаем parentId по умолчанию
+  // },
   {
     path: '/:catchAll(.*)',
     name: 'Error404',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,166 @@
 <template>
   <v-container>
-    <!-- Форма для ввода данных -->
-    <v-form>
-      <!-- Поле для категории -->
-      <v-text-field label="Категория" v-model="entry.category" />
+    <v-row>
+      <v-col cols="12">
+        <div class="flex">
+          <h2 class="text-h5">Финансовые Записи</h2>
+          <v-spacer />
+          <v-btn color="primary" class="mb-2" @click="startAdding">
+            Добавить Запись
+          </v-btn>
+        </div>
+        <v-dialog v-model="showDialog" persistent max-width="600px">
+          <v-card>
+            <v-card-title>{{ dialogTitle }}</v-card-title>
+            <v-card-text>
+              <v-text-field label="Категория" v-model="entry.category" />
+              <v-text-field label="Сумма" v-model="entry.amount" type="number" />
+              <v-text-field label="Пояснение" v-model="entry.description" />
+              <v-text-field label="Дата" v-model="entry.date" readonly />
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="showDialog = false">
+                Отмена
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="saveEntry">
+                Сохранить
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
-      <!-- Поле для суммы -->
-      <v-text-field label="Сумма" v-model="entry.amount" type="number" />
-
-      <!-- Поле для пояснения -->
-      <v-textarea label="Пояснение" v-model="entry.description" />
-
-      <!-- Выбор даты -->
-      <v-menu v-model="menu" close-on-content-click="false" nudge-right="40" offset-y min-width="290px">
-        <template v-slot:activator="{ attrs, on }">
-          <v-text-field v-model="entry.date" label="Дата" readonly v-bind="attrs" v-on="on" />
-        </template>
-        <v-date-picker v-model="entry.date" no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn text color="primary" @click="menu = false">Отмена</v-btn>
-          <v-btn text color="primary" @click="saveDate">OK</v-btn>
-        </v-date-picker>
-      </v-menu>
-
-      <!-- Кнопка добавления данных -->
-      <v-btn color="primary" @click="addEntry">Добавить</v-btn>
-    </v-form>
-
-    <!-- Таблица для отображения данных -->
-    <v-data-table :headers="headers" :items="entries" items-per-page="5" class="elevation-1" />
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-left">№</th>
+            <th class="text-left">Категория</th>
+            <th class="text-left">Сумма</th>
+            <th class="text-left">Пояснение</th>
+            <th class="text-left">Дата</th>
+            <th class="text-left">Действия</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(entry, index) in financeEntries" :key="entry.id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ entry.category }}</td>
+            <td>{{ entry.amount }}</td>
+            <td>{{ entry.description }}</td>
+            <td>{{ entry.date }}</td>
+            <td>
+              <v-btn icon @click="startEditing(entry)">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-btn icon @click="deleteEntry(entry)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
 <script>
-import { apiFinance } from '@/api';
+import { getFinanceEntries, updateFinanceEntry, addFinanceEntry, deleteFinanceEntry } from '@/api'
+
 export default {
   data() {
     return {
+      financeEntries: [],
+      showDialog: false,
+      dialogTitle: '',
       entry: {
         category: '',
         amount: 0,
         description: '',
-        date: new Date().toISOString().substr(0, 10),
+        date: new Date().toISOString().substr(0, 10)
       },
-      entries: [],
-      headers: [
-        { text: 'Дата', value: 'date' },
-        { text: 'Категория', value: 'category' },
-        { text: 'Сумма', value: 'amount' },
-        { text: 'Пояснение', value: 'description' },
-      ],
-      menu: false,
-    };
-  },
-  async created() {
-    try {
-      this.entries = await apiFinance.fetchEntries();
-    } catch (error) {
-      console.error('Ошибка при загрузке данных:', error);
+      editingEntry: null,
     }
   },
   methods: {
-    async addEntry() {
+    async getData() {
       try {
-        const newEntry = await apiFinance.addEntry(this.entry);
-        this.entries.push(newEntry);
-        this.entries.sort((a, b) => new Date(b.date) - new Date(a.date));
-        this.clearForm();
+        this.financeEntries = await getFinanceEntries()
       } catch (error) {
-        console.error('Ошибка при добавлении записи:', error);
+        console.error('Error fetching data:', error)
       }
     },
-    clearForm() {
-      this.entry = { category: '', amount: 0, description: '', date: new Date().toISOString().substr(0, 10) };
+    startAdding() {
+      this.dialogTitle = 'Новая Запись'
+      this.entry = {
+        category: '',
+        amount: 0,
+        description: '',
+        date: new Date().toISOString().substr(0, 10)
+      }
+      this.editingEntry = null
+      this.showDialog = true
     },
-    saveDate() {
-      this.menu = false;
-    }
+    startEditing(entry) {
+      this.dialogTitle = 'Редактировать Запись'
+      this.entry = { ...entry }
+      this.editingEntry = entry
+      this.showDialog = true
+    },
+    async saveEntry() {
+      if (this.entry.category && this.entry.amount && this.entry.description && this.entry.date) {
+        try {
+          if (this.editingEntry) {
+            // Обновление существующей записи
+            const updatedEntry = { ...this.entry }
+            const result = await updateFinanceEntry(this.editingEntry.id, updatedEntry)
+
+            if (result) {
+              // Обновление данных в массиве financeEntries
+              this.financeEntries = this.financeEntries.map(e =>
+                e.id === this.editingEntry.id ? updatedEntry : e
+              )
+            }
+          } else {
+            // Добавление новой записи
+            const newEntry = await addFinanceEntry(this.entry)
+            if (newEntry && newEntry.id) {
+              this.financeEntries.push(newEntry)
+            } else {
+              throw new Error('Failed to add new entry')
+            }
+          }
+        } catch (error) {
+          console.error('Error saving finance entry:', error)
+        }
+
+        // Сброс данных формы
+        this.entry = {
+          category: '',
+          amount: 0,
+          description: '',
+          date: new Date().toISOString().substr(0, 10)
+        }
+        this.showDialog = false
+        this.editingEntry = null
+      } else {
+        alert('Пожалуйста, заполните все поля')
+      }
+    },
+    async deleteEntry(entry) {
+      const confirmDelete = confirm(`Вы уверены, что хотите удалить запись: ${entry.category}?`)
+      if (confirmDelete) {
+        try {
+          await deleteFinanceEntry(entry.id)
+          this.financeEntries = this.financeEntries.filter(e => e.id !== entry.id)
+        } catch (error) {
+          console.error('Error deleting finance entry:', error)
+        }
+      }
+    },
   },
-};
+  mounted() {
+    this.getData()
+  },
+}
 </script>
