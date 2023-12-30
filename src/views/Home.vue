7fb2d4c95bacc8998d5fdb023925949d14699995
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-30 21:31:07 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,56 @@
 <template>
   <v-container>
-    <h1>Props</h1>
-    <!-- Блоки для отображения отформатированного и подсвеченного кода -->
-    <pre><code class="language-html">{{ formattedChildComponentCode }}</code></pre>
-    <pre><code class="language-html">{{ formattedParentComponentCode }}</code></pre>
+    <v-form>
+      <v-text-field label="Категория" v-model="entry.category"></v-text-field>
+      <v-text-field label="Сумма" v-model="entry.amount" type="number"></v-text-field>
+      <v-textarea label="Пояснение" v-model="entry.description"></v-textarea>
+      <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="290px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field v-model="entry.date" label="Дата" readonly v-bind="attrs" v-on="on"></v-text-field>
+        </template>
+        <v-date-picker v-model="entry.date" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false">Отмена</v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(entry.date)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+      <v-btn color="primary" @click="addEntry">Добавить</v-btn>
+    </v-form>
+    <!-- Таблица данных -->
+    <v-data-table :headers="headers" :items="entries" :items-per-page="5" class="elevation-1"></v-data-table>
   </v-container>
 </template>
 
 <script>
-import Prism from 'prismjs'
-import Prettier from 'prettier/standalone'
-import parserHtml from 'prettier/parser-html'
-import parserBabel from 'prettier/parser-babel'
-import childComponentCode from '@/views/source/childComponentCode.js'
-import parentComponentCode from '@/views/source/parentComponentCode.js'
-import 'prismjs/themes/prism-tomorrow.css'
-
 export default {
-  name: 'Home',
   data() {
     return {
-      formattedChildComponentCode: '',
-      formattedParentComponentCode: '',
-    }
-  },
-  async mounted() {
-    this.formattedChildComponentCode = await this.formatCode(childComponentCode)
-    this.formattedParentComponentCode =
-      await this.formatCode(parentComponentCode)
-    this.$nextTick(() => {
-      Prism.highlightAll()
-    })
+      entry: {
+        category: '',
+        amount: 0,
+        description: '',
+        date: new Date().toISOString().substr(0, 10),
+      },
+      entries: [],
+      headers: [
+        { text: 'Дата', value: 'date' },
+        { text: 'Категория', value: 'category' },
+        { text: 'Сумма', value: 'amount' },
+        { text: 'Пояснение', value: 'description' },
+      ],
+      menu: false,
+    };
   },
   methods: {
-    async formatCode(code) {
-      try {
-        return await Prettier.format(code, {
-          parser: 'babel', // Используйте 'vue' или 'html' для соответствующего типа кода
-          plugins: [parserBabel, parserHtml],
-        })
-      } catch (error) {
-        console.error('Ошибка форматирования:', error)
-        return code // В случае ошибки возвращаем исходный код
-      }
+    addEntry() {
+      this.entries.push({ ...this.entry });
+      this.entries.sort((a, b) => new Date(b.date) - new Date(a.date)); // Сортировка по дате
+      this.clearForm();
+    },
+    clearForm() {
+      this.entry = { category: '', amount: 0, description: '', date: new Date().toISOString().substr(0, 10) };
     },
   },
-}
+};
 </script>
 
-<style>
-/* Дополнительные стили, если необходимо */
-</style>
