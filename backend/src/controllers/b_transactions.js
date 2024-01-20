b4ec33d927b07f64d39f4c1c08d5651728ecec07
Isanin Sergey <sergey.isanin@gmail.com>
2024-01-20 18:52:05 +0300

    +

--- backend/src/controllers/b_transactions.js
+++ backend/src/controllers/b_transactions.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg')
 const { getNetworkDetails } = require('../db_type')
 const config = require('../config')
+const faker = require('faker')
 
 const networkDetails = getNetworkDetails()
 const dbConfig =
@@ -10,6 +11,29 @@ const dbConfig =
 
 // Создание пула соединений с базой данных
 const pool = new Pool(dbConfig)
+
+async function fillTransactionsWithRandomData(req, res) {
+  try {
+    const insertQueries = []
+    for (let i = 0; i < 10; i++) {
+      // Генерация 100 записей
+      const amount = faker.random.number({ min: -10000, max: 10000 }) // Случайное значение от -10000 до 10000
+      const comment = faker.lorem.sentence()
+      const category = faker.commerce.productName() // Используйте подходящую категорию
+      const timestamp = faker.date.recent(90).toISOString() // Дата за последние 90 дней
+
+      const query = `INSERT INTO dbo.transactions (amount, comment, category, timestamp)
+                           VALUES (${amount}, '${comment}', '${category}', '${timestamp}')`
+      insertQueries.push(pool.query(query))
+    }
+
+    await Promise.all(insertQueries)
+    res.send('Random data inserted successfully')
+  } catch (error) {
+    console.error('Error while inserting random transactions:', error)
+    res.status(500).send(error.message)
+  }
+}
 
 // Функция для получения транзакций за текущий месяц
 async function getTransactionsForCurrentMonth(req, res) {
@@ -56,4 +80,5 @@ async function getAllTransactions(req, res) {
 module.exports = {
   getTransactionsForCurrentMonth,
   getAllTransactions,
+  fillTransactionsWithRandomData,
 }
--- backend/src/routers.js
+++ backend/src/routers.js
@@ -6,11 +6,13 @@ const loginController = require('./controllers/b_login')
 const {
   getTransactionsForCurrentMonth,
   getAllTransactions,
+  fillTransactionsWithRandomData,
 } = require('./controllers/b_transactions')
 
 // Маршруты для аутентификации
 router.get('/database-info', loginController.getDatabaseInfo)
 router.get('/transactions/current-month', getTransactionsForCurrentMonth)
 router.get('/transactions/all', getAllTransactions)
+router.get('/faker', fillTransactionsWithRandomData)
 
 module.exports = router
