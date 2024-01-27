8c5a03b27bda7b134c2eb73720f6165728fe2e37
Isanin Sergey <sergey.isanin@gmail.com>
2024-01-27 09:55:21 +0300

    <title>MY FINANCE</title>

--- src/modules/tool/components/tabs/FinanceTable.vue
+++ src/modules/tool/components/tabs/FinanceTable.vue
@@ -6,7 +6,10 @@
           <thead>
             <tr>
               <th class="text-left">№</th>
+              <th class="text-left">id</th>
               <th class="text-left">Сумма</th>
+              <th class="text-left">Мой комментарий</th>
+              <th class="text-left">Моя категория</th>
               <th class="text-left">Комментарий</th>
               <th class="text-left">Категория</th>
               <th class="text-left">Время</th>
@@ -17,8 +20,12 @@
             <tr
               v-for="(transaction, index) in transactions"
               :key="transaction.transaction_id"
+              :class="{ alternateBackground: shouldAlternateBackground(index) }"
             >
               <td :style="{ color: 'gray' }">{{ index + 1 }}</td>
+              <td :style="{ color: 'gray' }">
+                {{ transaction.transaction_id }}
+              </td>
               <td
                 :style="{
                   color: transaction.operation_amount >= 0 ? 'green' : 'red',
@@ -27,13 +34,15 @@
                 {{ transaction.operation_amount }}
                 {{ transaction.operation_currency }}
               </td>
+              <td>{{ transaction.my_description }}</td>
+              <td>{{ transaction.my_category }}</td>
               <td>{{ transaction.description }}</td>
               <td>{{ transaction.category }}</td>
               <td>
                 {{ formatTime(transaction.date_of_operation) }}
               </td>
-              <td :style="{ color: 'gray' }">
-                {{ formatDate(transaction.date_of_operation) }}
+              <td :style="dayOfWeekStyle(transaction.date_of_operation)">
+                {{ formatDayWeek(transaction.date_of_operation) }}
               </td>
             </tr>
           </tbody>
@@ -44,13 +53,21 @@
 </template>
 
 <script>
-import { markRaw } from 'vue'
 import { transactionsApi } from '../../api/transactions'
 import { format, parseISO } from 'date-fns'
 
 export default {
   data() {
     return {
+      weekDays: {
+        Mon: 'ПН',
+        Tue: 'ВТ',
+        Wed: 'СР',
+        Thu: 'ЧТ',
+        Fri: 'ПТ',
+        Sat: 'СБ',
+        Sun: 'ВС',
+      },
       transactions: [],
     }
   },
@@ -58,7 +75,39 @@ export default {
     selectedYear: Number,
     selectedMonth: Number,
   },
+  computed: {
+    alternatedBackgrounds() {
+      return this.transactions.map((transaction, index, array) => {
+        if (index === 0) return false
+        const prevDate = format(
+          parseISO(array[index - 1].date_of_operation),
+          'yyyy-MM-dd'
+        )
+        const currentDate = format(
+          parseISO(transaction.date_of_operation),
+          'yyyy-MM-dd'
+        )
+        return prevDate !== currentDate
+      })
+    },
+  },
   methods: {
+    dayOfWeekStyle(timestamp) {
+      const dayOfWeek = this.formatDayWeek(timestamp)
+      if (dayOfWeek === 'СБ' || dayOfWeek === 'ВС') {
+        return { color: 'red' }
+      }
+      return { color: 'white' }
+    },
+    shouldAlternateBackground(index) {
+      return this.alternatedBackgrounds[index]
+    },
+    formatDayWeek(timestamp) {
+      if (!timestamp || !Date.parse(timestamp)) return 'Неверная дата'
+      const date = parseISO(timestamp)
+      const dayOfWeek = format(date, 'EEE')
+      return this.weekDays[dayOfWeek] || dayOfWeek
+    },
     formatDate(timestamp) {
       if (!timestamp || !Date.parse(timestamp)) return 'Неверная дата'
       const date = parseISO(timestamp)
@@ -114,3 +163,14 @@ export default {
   },
 }
 </script>
+
+<style>
+.alternateBackground {
+  background-color: rgba(
+    255,
+    255,
+    255,
+    0.1
+  ); /* Или любой другой цвет, подходящий для темной темы */
+}
+</style>
