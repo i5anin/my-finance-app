094f28ea41aa02cfbc06bf6de4d77bdcf062cc05
Isanin Sergey <sergey.isanin@gmail.com>
2024-06-16 12:08:40 +0300

    Update Table.vue

--- src/modules/kombat/components/Table.vue
+++ src/modules/kombat/components/Table.vue
@@ -2,9 +2,7 @@
   <v-table>
     <thead>
       <tr>
-        <th v-for="column in columns" :key="column.value">
-          {{ column.text }}
-        </th>
+        <th v-for="column in columns" :key="column.value">{{ column.text }}</th>
       </tr>
     </thead>
     <tbody>
@@ -13,17 +11,22 @@
           <template v-if="column.isImage">
             <img :src="row[column.value]" alt="Изображение" />
           </template>
+          <template v-else-if="column.formatter">
+            <!-- Использование prefix и postfix напрямую в шаблоне с проверкой на их наличие -->
+            <template v-if="column.prefix">{{ column.prefix }}</template>
+            {{ column.formatter(row[column.value]) }}
+            <template v-if="column.postfix">{{ column.postfix }}</template>
+          </template>
           <template v-else>
-            <!-- Префиксы и постфиксы для значений, если необходимо -->
-            {{ column.prefix || '' }}
-            {{ row[column.value] }}
-            {{ column.postfix || '' }}
+            {{ column.prefix || '' }}{{ row[column.value]
+            }}{{ column.postfix || '' }}
           </template>
         </td>
       </tr>
     </tbody>
   </v-table>
 </template>
+
 <script>
 import { kombatApi } from '../api/kombat'
 
@@ -31,16 +34,32 @@ export default {
   name: 'Table',
   data() {
     return {
-      // Определите структуру колонок в виде массива объектов
       columns: [
         { text: 'Название', value: 'name' },
         { text: 'Местоположение', value: 'location' },
-        { text: 'Уровень', value: 'level' },
-        { text: 'Цена', value: 'price', prefix: '🪙' },
-        { text: 'Прибыль в час', value: 'profit', postfix: ' 🪙/час' },
-        { text: 'Окупаемость', value: 'price_profit_ratio' },
-
-        // { text: 'Изображение', value: 'img', isImage: true },
+        {
+          text: 'Уровень',
+          value: 'level',
+          formatter: (value) => this.formatNumber(value),
+        },
+        {
+          text: 'Цена',
+          value: 'price',
+          prefix: '🪙',
+          formatter: (value) => this.formatNumber(value),
+        },
+        {
+          text: 'Прибыль в час',
+          value: 'profit',
+          postfix: ' 🪙/час',
+          formatter: (value) => this.formatNumber(value),
+        },
+        // Изменил форматирование Окупаемости
+        {
+          text: 'Окупаемость',
+          value: 'price_profit_ratio',
+          formatter: (value) => Math.round(value),
+        },
       ],
       rows: [],
     }
@@ -48,14 +67,20 @@ export default {
   methods: {
     async fetchKombatData() {
       try {
+        // Попытка получения данных от API
         const data = await kombatApi.getKombat()
         this.rows = data
       } catch (error) {
         console.error('Ошибка получения данных: ', error)
       }
     },
+    formatNumber(value) {
+      // Форматирование чисел с пробелом в качестве разделителя групп
+      return value.toLocaleString('ru-RU')
+    },
   },
   mounted() {
+    // Получаем данные при монтировании компонента
     this.fetchKombatData()
   },
 }
